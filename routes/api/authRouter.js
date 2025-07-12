d42c87ced0fc77a2e297ca30b5a7614e1ed2323d
Charles-West-Africa <charlesuwem94@gmail.com>
2025-07-12 16:46:49 +0100

    Add json tokens lib, create and verify tokens. Add login and Logout endpoints

--- routes/api/authRouter.js
+++ routes/api/authRouter.js
@@ -1,7 +1,9 @@
 import { Router } from "express";
 const router = Router();
-import { register } from "../controllers/authController.js";
+import { register, login, logout } from "../controllers/authController.js";
 
 router.post("/register", register);
+router.post("/login", login);
+router.get("/logout", logout);
 
 export default router;
