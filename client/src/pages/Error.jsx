9de3a51cb0d85c520cb10cc036000ba05b8a6b7b
Charles-West-Africa <charlesuwem94@gmail.com>
2025-07-14 16:56:10 +0100

    Add Error page

---
+++ client/src/pages/Error.jsx
@@ -0,0 +1,26 @@
+import { useRouteError } from "react-router-dom";
+import Wrapper from "../assets/wrappers/ErrorPage";
+import img from "../assets/images/not-found.svg";
+
+const Error = () => {
+  const error = useRouteError();
+  if (error.status === 404) {
+    return (
+      <Wrapper>
+        <div>
+          <img src={img} alt="not found" />
+          <h3>ohh! page not found</h3>
+          <p>We can&apos;t seem to find the page you are looking for</p>
+        </div>
+      </Wrapper>
+    );
+  }
+
+  return (
+    <div>
+      <h3>Something went wrong!!!</h3>
+    </div>
+  );
+};
+
+export default Error;
