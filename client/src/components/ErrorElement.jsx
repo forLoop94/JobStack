65814a065556bbda76696e0993285f34460461e5
Charles-West-Africa <charlesuwem94@gmail.com>
2025-07-14 16:47:27 +0100

    Create error component, add react toastify for toast notifications

---
+++ client/src/components/ErrorElement.jsx
@@ -0,0 +1,9 @@
+import { useRouteError } from "react-router-dom";
+
+const ErrorElement = () => {
+  const error = useRouteError();
+  console.log(error);
+
+  return <div>There was an error!!!{error}</div>;
+};
+export default ErrorElement;
