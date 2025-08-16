d14dcdeefd32c8b1d5582ed3f60ba4b4ec59fe46
Charles-West-Africa <charlesuwem94@gmail.com>
2025-08-16 23:03:23 +0100

    Create route object in App file, use imported loaders and actions as onLoad functions, provide staletime from tanstackQuery lib

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,131 @@
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+// import queries
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import {
+  HomeLayout,
+  Landing,
+  Register,
+  Login,
+  DashboardLayout,
+  Error,
+  AddJob,
+  Stats,
+  AllJobs,
+  Profile,
+  Admin,
+  EditJob,
+} from "./pages";
 
-function App() {
+import { action as registerAction } from "./pages/Register";
+import { action as loginAction } from "./pages/Login";
+import { loader as dashboardLoader } from "./pages/DashboardLayout";
+import { action as addJobAction } from "./pages/AddJob";
+import { loader as allJobsLoader } from "./pages/AllJobs";
+import { loader as editJobLoader } from "./pages/EditJob";
+import { action as editJobAction } from "./pages/EditJob";
+import { action as deleteJobAction } from "./pages/DeleteJob";
+import { loader as adminLoader } from "./pages/Admin";
+import { action as profileAction } from "./pages/Profile";
+import { loader as StatsLoader } from "./pages/Stats";
+import ErrorElement from "./components/ErrorElement";
+
+export const checkDefaultTheme = () => {
+  const isDarkTheme = localStorage.getItem("darkTheme") === "true";
+  document.body.classList.toggle("dark-theme", isDarkTheme);
+  return isDarkTheme;
+};
+
+const isDarkThemeEnabled = checkDefaultTheme();
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 5,
+    },
+  },
+});
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomeLayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Landing />,
+      },
+      {
+        path: "register",
+        element: <Register />,
+        action: registerAction,
+      },
+      {
+        path: "login",
+        element: <Login />,
+        action: loginAction(queryClient),
+      },
+      {
+        path: "dashboard",
+        element: (
+          <DashboardLayout
+            isDarkThemeEnabled={isDarkThemeEnabled}
+            queryClient={queryClient}
+          />
+        ),
+        loader: dashboardLoader(queryClient),
+        children: [
+          {
+            index: true,
+            element: <AddJob />,
+            action: addJobAction(queryClient),
+          },
+          {
+            path: "stats",
+            element: <Stats />,
+            loader: StatsLoader(queryClient),
+            errorElement: <ErrorElement />,
+          },
+          {
+            path: "all-jobs",
+            element: <AllJobs />,
+            loader: allJobsLoader(queryClient),
+            errorElement: <ErrorElement />,
+          },
+          {
+            path: "profile",
+            element: <Profile />,
+            action: profileAction(queryClient),
+          },
+          {
+            path: "admin",
+            element: <Admin />,
+            loader: adminLoader,
+          },
+          {
+            path: "edit-job/:id",
+            element: <EditJob />,
+            loader: editJobLoader(queryClient),
+            action: editJobAction(queryClient),
+          },
+          {
+            path: "delete-job/:id",
+            action: deleteJobAction(queryClient),
+          },
+        ],
+      },
+    ],
+  },
+]);
+
+const App = () => {
   return (
-    <>
-      <h1>App</h1>
-    </>
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />;
+      <ReactQueryDevtools InitialIsOpen={false} />;
+    </QueryClientProvider>
   );
-}
+};
 
 export default App;
