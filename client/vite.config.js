d6bb921b0e78bdf6a7bfc3e87e2045a8a9a2aba5
Charles-West-Africa <charlesuwem94@gmail.com>
2025-11-20 23:20:42 +0100

    Add rollup options to vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,10 +4,17 @@ import react from "@vitejs/plugin-react-swc";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {},
+      },
+    },
+  },
   server: {
     proxy: {
       "/api": {
-        target: "https://jobify-stnc.onrender.com/api",
+        target: "http://localhost:3000",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
         onError: (err) => {
